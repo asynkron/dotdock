42b5f46023ed5319a5b96e864aa42b4d89807e47
rogerjohansson <rogeralsing@gmail.com>
2020-09-20 20:24:39 +0200

    first

---
+++ dotdock/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using CommandLine;
+using net.r_eg.MvsSln;
+
+namespace dotdock
+{
+    class Program
+    {
+        public class Options
+        {
+            [Option('s', "sln", Required = true, HelpText = "Solution file to be processed.")]
+            public string SolutionPath { get; set; }
+            
+            
+        }
+
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(RunOptions)
+                .WithNotParsed(HandleParseError);
+        }
+
+        static void RunOptions(Options opts)
+        {
+            var template = @"
+FROM mcr.microsoft.com/dotnet/core/sdk:3.1 AS build-env
+WORKDIR /app
+
+# Copy nugetconfigs 
+%COPYNUGET%
+# Copy csprojs 
+%COPYCSPROJ%
+# Restore project
+%DOTNETRESTORE%
+
+# Copy everything else and build
+COPY . ./
+%DOTNETPUBLISH%
+
+# Build runtime image
+FROM mcr.microsoft.com/dotnet/core/sdk:3.1
+WORKDIR /app
+COPY --from=build-env /app/out .
+%ENTRYPOINT%
+
+".Replace("'", "\"");
+
+     
+            var slnPath = opts.SolutionPath.Trim();
+            var solutionFiles = Directory.GetFiles(slnPath, "*.sln");
+            var solutionFile=SelectOne(solutionFiles, s=>s,"Select Solution file");
+            
+            
+            var path = Path.GetDirectoryName(solutionFile);
+            Console.WriteLine(path);
+
+            using var sln = new Sln(solutionFile, SlnItems.Projects);
+
+            var nugetConfigs = Directory.EnumerateFiles(path, "nuget.config", SearchOption.AllDirectories);
+            var copyNugetConfigs = DockerCopyItems(nugetConfigs);
+            template = template.Replace("%COPYNUGET%", copyNugetConfigs);
+
+            var projects =
+                sln
+                    .Result
+                    .ProjectItems
+                    .ToList();
+
+            
+            var projectFile = SelectOne(projects, p => p.path,"Select project to run");
+
+            var paths = projects.Select(p => p.path.Replace("\\", "/"));
+            var copyProjects = DockerCopyItems(paths);
+
+            template = template.Replace("%COPYCSPROJ%", copyProjects);
+
+
+            var runRestore = $"RUN dotnet restore \"{projectFile.path.Replace("\\", "/")}\"";
+            template = template.Replace("%DOTNETRESTORE%", runRestore);
+
+            var runPublish = $"RUN dotnet publish -c Release -o out \"{projectFile.path.Replace("\\", "/")}\"";
+            template = template.Replace("%DOTNETPUBLISH%", runPublish);
+
+            var app = Path.GetFileNameWithoutExtension(projectFile.path).Replace("\\", "/");
+            app = Path.GetFileName(app);
+            var entrypoint = "ENTRYPOINT [\"dotnet\", \"" + app + ".dll\"]";
+            template = template.Replace("%ENTRYPOINT%", entrypoint);
+
+            var dockerFilePath = Path.Combine(path, "dockerfile");
+            File.WriteAllText(dockerFilePath,template);
+            Console.WriteLine(template);
+
+
+            //handle options
+        }
+
+        private static string DockerCopyItems(IEnumerable<string> paths)
+        {
+            var sb = new StringBuilder();
+            foreach (var p in paths)
+            {
+                sb.AppendLine($"COPY {p} ./{p}");
+            }
+
+            return sb.ToString();
+        }
+
+        private static T SelectOne<T>(IEnumerable<T> options, Func<T,string> stringer,string prompt = "Select option")
+        {
+            Console.WriteLine();
+            var arr = options.ToArray();
+            if (arr.Length == 1)
+            {
+                var onlyOne = arr.First();
+                Console.WriteLine($"Using {stringer(onlyOne)}");
+                return onlyOne;
+            }
+            for (var i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}) {stringer(arr[i])}");
+            }
+
+            Console.WriteLine();
+            while (true)
+            {
+                
+                Console.WriteLine(prompt + "> ");
+                var res = Console.ReadLine();
+                if (int.TryParse(res!.Trim(), out var index) && index >= 1 && index <= arr.Length )
+                {
+                    return arr[index-1];
+                }
+            }
+        }
+
+        static void HandleParseError(IEnumerable<Error> errs)
+        {
+            //handle errors
+        }
+
+    }
+}
